32c83ee709cdd9730fbd0123aeb743aadbe64792
shreyashbhise <shreyashbhise581@gmail.com>
2023-07-12 13:01:48 -0400

    file is added

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -10,10 +10,10 @@ echo "AWS Account ID: $aws_account_id"
 
 # Set AWS region and bucket name
 aws_region="us-east-1"
-bucket_name="abhishek-ultimate-bucket"
+bucket_name="shreyash-ultimate-bucket"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
-email_address="zyz@gmail.com"
+email_address="shreyashbhise581@gmail.com"
 
 # Create IAM Role for the project
 role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
